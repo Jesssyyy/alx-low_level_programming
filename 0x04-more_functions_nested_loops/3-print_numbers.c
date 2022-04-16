006cd86056cb0795391ea7425c7017702bb161cd
Jesssyyy <jessieabonoje@gmail.com>
2022-04-15 20:09:42 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - prints the numbers,from 0 to 9,
+ * followed by a new line
+ */
+
+void print_number(void)
+{
+
+int i;
+
+i = 0;
+
+for (i =0; i < 10; i++)
+
+_putchar(i + '0');
+
+_putchar ('\n');
+}
