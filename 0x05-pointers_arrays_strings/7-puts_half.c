fce01ac9c50a73e274ea5b6c37a2059f9d795315
Jesssyyy <jessieabonoje@gmail.com>
2022-04-21 19:42:01 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half sting
+ * @str: string
+ *
+ * Return : nothing
+ */
+void puts_half(char *str)
+{
+int i = 0;
+int j = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+for (j = 0; str[j] != '\0'; j++)
+}
+if (j >= i / 2)
+putchar(str[j]);
+}
+}
+else
+{
+for (j = 0; str[j] != '\0'; j++)
+{
+if (j > i / 2)
+putchar(str[j]);
+}
+}
+putchar('\n');
+}
