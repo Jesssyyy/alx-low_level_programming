140e92808d2a9341b005a66e2aa3f3aa6a03ff2d
Jesssyyy <jessieabonoje@gmail.com>
2022-05-02 19:42:38 -0700

    Task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints its name, followed by a new inr.
- * @argc: number of command ine arguments.
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
