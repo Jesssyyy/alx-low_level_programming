6c98b3015876902ff863a138868a9139881449a2
Jesssyyy <jessieabonoje@gmail.com>
2022-05-09 19:54:11 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *relloc;
+	char *clone, *realloc;
 	unsigned int i;
 
 	if (ptr != NULL)
@@ -24,8 +24,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (0) ; }
 	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		*(relloc + i) + clone[i];
+		*(realloc + i) + clone[i];
 	}
 	free(ptr);
-	return (relloc);
+	return (realloc);
 }
