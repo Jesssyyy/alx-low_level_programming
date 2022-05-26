e0045801191dce95b3557664edbc3d130f04ddf8
Jesssyyy <jessieabonoje@gmail.com>
2022-05-25 19:32:14 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * list_len - returns then numbers of elements in a list
+ * @h: singly linked list
+ * Return: number of elements in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nelem;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
+}
