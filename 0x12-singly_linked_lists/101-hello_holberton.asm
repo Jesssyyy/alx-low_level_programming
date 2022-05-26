608b087ae64e44916d60bee7b81124d21fc36a4d
Jesssyyy <jessieabonoje@gmail.com>
2022-05-25 20:17:44 -0700

    Task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+            SECTION .data
+msg:        db "Hellow, holberton", 0
+fmt:        db "s%", 10, 0
+
+
+            SECTION .text
+            extern printf
+            global main
+main:
+            mov esi, msg
+            mov adi, fmt
+            mov eax, 0
+            call printf
+
+
+            mov eax, 0
+            ret       
